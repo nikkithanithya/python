cefbe9bbc6ff2a55d73e03074d7b9af535d9c769
lakshmi <lakshmigowda83174@gmail.com>
2022-11-09 03:54:55 -0800

    first commit

---
+++ Desktop/dhanu/history.py
@@ -0,0 +1,140 @@
+# -*- coding: utf-8 -*-
+# *** Spyder Python Console History Log ***
+
+## ---(Mon Oct 10 21:53:31 2022)---
+runfile('C:/Users/SPTINT-14/.spyder-py3/temp.py', wdir='C:/Users/SPTINT-14/.spyder-py3')
+
+## ---(Mon Oct 10 21:55:37 2022)---
+runfile('C:/Users/SPTINT-14/.spyder-py3/temp.py', wdir='C:/Users/SPTINT-14/.spyder-py3')
+runfile('D:/Nithya/simple calculator.py', wdir='D:/Nithya')
+runfile('D:/Nithya/simple calculator.py', wdir='D:/Nithya')
+runfile('D:/Nithya/simple calculator.py', wdir='D:/Nithya')
+runfile('D:/Nithya/simple calculator.py', wdir='D:/Nithya')
+runfile('D:/Nithya/untitled1.py', wdir='D:/Nithya')
+runfile('D:/Nithya/simple calculator.py', wdir='D:/Nithya')
+runfile('D:/Nithya/untitled2.py', wdir='D:/Nithya')
+runfile('D:/Nithya/healthy or not.py', wdir='D:/Nithya')
+runfile('D:/Nithya/healthy or not.py', wdir='D:/Nithya')
+runfile('D:/Nithya/healthy or not.py', wdir='D:/Nithya')
+runfile('C:/Users/SPTINT-14/.spyder-py3/temp.py', wdir='C:/Users/SPTINT-14/.spyder-py3')
+runfile('C:/Users/SPTINT-14/.spyder-py3/temp.py', wdir='C:/Users/SPTINT-14/.spyder-py3')
+runcell(0, 'C:/Users/SPTINT-14/.spyder-py3/temp.py')
+runfile('C:/Users/SPTINT-14/.spyder-py3/temp.py', wdir='C:/Users/SPTINT-14/.spyder-py3')
+runfile('D:/Nithya/healthy or not.py', wdir='D:/Nithya')
+
+## ---(Tue Oct 11 22:41:46 2022)---
+runfile('D:/Nithya/healthy or not.py', wdir='D:/Nithya')
+
+## ---(Wed Oct 19 00:22:12 2022)---
+runfile('D:/Nithya/healthy or not.py', wdir='D:/Nithya')
+
+## ---(Wed Oct 19 01:43:20 2022)---
+runfile('D:/Nithya/healthy or not.py', wdir='D:/Nithya')
+runfile('D:/Nithya/untitled0.py', wdir='D:/Nithya')
+runfile('C:/Users/SPTINT-14/.spyder-py3/temp.py', wdir='C:/Users/SPTINT-14/.spyder-py3')
+
+## ---(Wed Oct 19 02:29:23 2022)---
+runfile('D:/Nithya/healthy or not.py', wdir='D:/Nithya')
+runfile('D:/Nithya/simple calculator.py', wdir='D:/Nithya')
+runfile('D:/Nithya/healthy or not.py', wdir='D:/Nithya')
+runfile('D:/Nithya/untitled0.py', wdir='D:/Nithya')
+runfile('D:/Nithya/untitled1.py', wdir='D:/Nithya')
+runfile('D:/Nithya/untitled2.py', wdir='D:/Nithya')
+runfile('D:/Nithya/lambda.py', wdir='D:/Nithya')
+
+## ---(Wed Oct 19 04:27:01 2022)---
+runfile('D:/Nithya/untitled0.py', wdir='D:/Nithya')
+runfile('D:/Nithya/untitled1.py', wdir='D:/Nithya')
+runfile('D:/Nithya/map.py', wdir='D:/Nithya')
+runfile('D:/Nithya/untitled1.py', wdir='D:/Nithya')
+runfile('D:/Nithya/lambda.py', wdir='D:/Nithya')
+runfile('D:/Nithya/reduce.py', wdir='D:/Nithya')
+runfile('D:/Nithya/filter.py', wdir='D:/Nithya')
+runfile('D:/Nithya/map.py', wdir='D:/Nithya')
+runfile('D:/Nithya/lambda.py', wdir='D:/Nithya')
+runfile('D:/Nithya/simple calculator.py', wdir='D:/Nithya')
+runfile('D:/Nithya/healthy or not.py', wdir='D:/Nithya')
+runfile('D:/Nithya/untitled2.py', wdir='D:/Nithya')
+runfile('D:/Nithya/untitled3.py', wdir='D:/Nithya')
+runfile('D:/Nithya/untitled2.py', wdir='D:/Nithya')
+runfile('D:/Nithya/map.py', wdir='D:/Nithya')
+runfile('D:/Nithya/reduce.py', wdir='D:/Nithya')
+runfile('D:/Nithya/untitled2.py', wdir='D:/Nithya')
+runfile('D:/Nithya/reduce.py', wdir='D:/Nithya')
+runfile('D:/Nithya/untitled2.py', wdir='D:/Nithya')
+runfile('D:/Nithya/untitled3.py', wdir='D:/Nithya')
+runfile('D:/Nithya/untitled4.py', wdir='D:/Nithya')
+
+## ---(Fri Oct 21 04:35:39 2022)---
+runfile('D:/Nithya/untitled0.py', wdir='D:/Nithya')
+
+## ---(Mon Oct 31 01:35:21 2022)---
+runfile('D:/Nithya/untitled0.py', wdir='D:/Nithya')
+runfile('D:/Nithya/untitled1.py', wdir='D:/Nithya')
+runfile('D:/Nithya/untitled2.py', wdir='D:/Nithya')
+runfile('D:/Nithya/untitled3.py', wdir='D:/Nithya')
+runfile('D:/Nithya/untitled4.py', wdir='D:/Nithya')
+runfile('D:/Nithya/untitled6.py', wdir='D:/Nithya')
+runfile('D:/Nithya/untitled1.py', wdir='D:/Nithya')
+runfile('D:/Nithya/untitled2.py', wdir='D:/Nithya')
+runfile('D:/Nithya/untitled1.py', wdir='D:/Nithya')
+runfile('D:/Nithya/untitled0.py', wdir='D:/Nithya')
+runfile('D:/Nithya/untitled1.py', wdir='D:/Nithya')
+runfile('D:/Nithya/untitled2.py', wdir='D:/Nithya')
+runfile('D:/dhanu/isnull.py', wdir='D:/dhanu')
+runfile('D:/dhanu/fillna.py', wdir='D:/dhanu')
+runfile('D:/dhanu/untitled7.py', wdir='D:/dhanu')
+runfile('D:/dhanu/untitled8.py', wdir='D:/dhanu')
+runfile('D:/dhanu/untitled7.py', wdir='D:/dhanu')
+
+## ---(Mon Oct 31 03:19:38 2022)---
+runfile('D:/dhanu/untitled8.py', wdir='D:/dhanu')
+runfile('D:/dhanu/untitled9.py', wdir='D:/dhanu')
+runfile('D:/dhanu/untitled10.py', wdir='D:/dhanu')
+runfile('D:/dhanu/untitled9.py', wdir='D:/dhanu')
+runfile('D:/dhanu/untitled10.py', wdir='D:/dhanu')
+runfile('D:/dhanu/untitled9.py', wdir='D:/dhanu')
+runfile('D:/dhanu/untitled10.py', wdir='D:/dhanu')
+runfile('D:/dhanu/untitled9.py', wdir='D:/dhanu')
+
+## ---(Fri Nov  4 01:54:48 2022)---
+runfile('D:/dhanu/untitled10.py', wdir='D:/dhanu')
+runfile('D:/dhanu/barchart.py', wdir='D:/dhanu')
+runfile('D:/dhanu/plot.py', wdir='D:/dhanu')
+runfile('D:/Nithya/untitled11.py', wdir='D:/Nithya')
+
+## ---(Sun Nov  6 22:04:08 2022)---
+runfile('D:/dhanu/untitled11.py', wdir='D:/dhanu')
+runfile('D:/dhanu/untitled12.py', wdir='D:/dhanu')
+runfile('D:/dhanu/untitled13.py', wdir='D:/dhanu')
+runfile('D:/dhanu/untitled12.py', wdir='D:/dhanu')
+runfile('D:/dhanu/untitled13.py', wdir='D:/dhanu')
+runfile('D:/dhanu/untitled11.py', wdir='D:/dhanu')
+runfile('D:/dhanu/untitled13.py', wdir='D:/dhanu')
+runfile('D:/dhanu/untitled12.py', wdir='D:/dhanu')
+runfile('D:/dhanu/untitled13.py', wdir='D:/dhanu')
+runfile('D:/dhanu/untitled12.py', wdir='D:/dhanu')
+runfile('D:/dhanu/untitled14.py', wdir='D:/dhanu')
+
+## ---(Mon Nov  7 00:46:18 2022)---
+runfile('D:/dhanu/untitled11.py', wdir='D:/dhanu')
+runfile('D:/dhanu/untitled13.py', wdir='D:/dhanu')
+runfile('D:/dhanu/untitled14.py', wdir='D:/dhanu')
+runfile('D:/dhanu/untitled15.py', wdir='D:/dhanu')
+runfile('D:/dhanu/untitled14.py', wdir='D:/dhanu')
+runfile('D:/dhanu/untitled13.py', wdir='D:/dhanu')
+runfile('D:/dhanu/untitled11.py', wdir='D:/dhanu')
+runfile('D:/dhanu/untitled16.py', wdir='D:/dhanu')
+
+## ---(Mon Nov  7 04:15:39 2022)---
+runfile('C:/Users/SPTINT-14/untitled0.py', wdir='C:/Users/SPTINT-14')
+
+## ---(Tue Nov  8 00:41:44 2022)---
+runfile('D:/dhanu/plot.py', wdir='D:/dhanu')
+
+## ---(Tue Nov  8 22:02:47 2022)---
+runfile('D:/dhanu/untitled0.py', wdir='D:/dhanu')
+
+## ---(Wed Nov  9 00:45:14 2022)---
+runfile('D:/Nithya/untitled0.py', wdir='D:/Nithya')
+runfile('D:/Nithya/untitled1.py', wdir='D:/Nithya')
